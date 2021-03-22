9884f3fd056ccbcb5a2175a6ecb948c8571f179a
Jordan <jordan-huitema@live.nmit.ac.nz>
2021-03-23 09:28:02 +1300

    added sample code to repo

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zoo = new Zoo();
+
+            var lion = new Lion()
+            {
+                Name = "Cleopatra"
+            }; 
+            zoo.Animals.Add(lion);
+
+            var tiger = new Tiger
+            {
+                Name = "Paprika"
+            };
+            zoo.Animals.Add(tiger);
+
+            var mouse = new Mouse
+            {
+                Name = "Hercules"
+            };
+
+            zoo.Animals.Add(mouse);
+
+            Console.WriteLine(zoo.About());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Polymorphism/Zoo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class Zoo
+    {
+        private readonly List<Animal> _animals;
+
+        public Zoo()
+        {
+            _animals = new List<Animal>();
+        }
+
+        public List<Animal> Animals { get => _animals; }
+
+        public string About()
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (Animal animal in _animals)
+            {
+                result.AppendLine(animal.About());
+            }
+            return result.ToString();
+        }
+    }
+}
